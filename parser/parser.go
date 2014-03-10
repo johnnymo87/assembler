c2b2269f0cc563c76a3c0ebac7936e1520c4f8f4
johnnymo87 <johnnymo87@gmail.com>
2014-03-10 16:14:11 +0000

    beginning the regexp of parser commands

--- parser/parser.go
+++ parser/parser.go
@@ -1,13 +1,10 @@
 package parser
 
 import (
-	"io/ioutil"
-	"strings"
+	"bufio"
+	"github.com/johnnymo87/assembler/commands"
+	"os"
 )
-
-type Parser struct {
-	Lines []string
-}
 
 func check(e error) {
 	if e != nil {
@@ -15,10 +12,40 @@ func check(e error) {
 	}
 }
 
-func NewParser(filename string) *Parser {
-	content, err := ioutil.ReadFile(filename)
-	check(err)
-	return &Parser{Lines: strings.Split(string(content), "\n")}
+type Parser interface {
+	HasMoreCommands() bool
+	ReadCommand() *Command
 }
+
+type Scanner struct {
+	scanner *bufio.Scanner
+}
+
+func NewScanner(filename string) *Scanner {
+	file, err := os.Open(filename)
+	check(err)
+	defer file.Close()
+	return &Parser{scanner: bufio.NewScanner(file)}
+}
+
+func (s *Scanner) HasMoreCommands() bool {
+	return s.scanner.Scan()
+}
+
+func (s *Scanner) ReadCommand() *Command {
+	return commands.NewCommand(s.scanner.Text())
+}
+
+//func NewParser(filename string) *Parser {
+//	file, err := os.Open(filename)
+//	check(err)
+//	defer file.Close()
+//	//var lines []string
+//	//for scanner.Scan() {
+//	//	lines = append(lines, scanner.Text())
+//	//}
+//	//check(scanner.Err())
+//	return &Parser{scanner: bufio.NewScanner(file)}
+//}
 
 //func hasMoreCommands() bool {
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -4,12 +4,29 @@ import (
 	. "github.com/johnnymo87/assembler/parser"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"os"
+	"bufio"
 )
 
+var _ = Describe("Packages", func() {
+	It("ioutil.Readfile", func() {
+		file, _ := os.Open("dummy_file.txt")
+		defer file.Close()
+		var lines []string
+		scanner := bufio.NewScanner(file)
+		
+		Expect(os.Open("dummy_file.txt")).To(Equal
+
+	})
+	It("strings.Split", func() {
+		content, _ := ioutil.ReadFile("dummy_file.txt")
+		Expect(strings.Fields(string(content))).To(Equal([]string{"0", "a"}))
+	})
+
+})
 var _ = Describe("Parser", func() {
-	It("should read a file", func() {
-		path := "dummy_file.txt"
-		p := NewParser(path)
-		Expect(p.Lines).To(Equal([]string{"0"}))
+	It("NewParser should read file lines into a slice", func() {
+		p := NewParser("dummy_file.txt")
+		Expect(p.Lines).To(Equal([]string{"0 1", "a"}))
 	})
 })
