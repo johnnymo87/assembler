cd32f602c9200fa37b270c0fb180fb92ef9e7601
johnnymo87 <johnnymo87@gmail.com>
2014-03-16 04:11:51 +0000

    added test for ToBinary()

--- code/code_test.go
+++ code/code_test.go
@@ -8,14 +8,24 @@ import (
 )
 
 var _ = Describe("Binary conversion", func() {
+	var a1 = Command("@16")
+	var c1 = Command("D=D-M")
+	var c2 = Command("D;JGT")
+	var lines = []Command{a1, c1, c2}
+	var bLines = []string{
+		"0000000000010000",
+		"1111010011010000",
+		"1110001100000001",
+	}
+
 	It("A_Binary()", func() {
-		a1 := Command("@16")
-		Ω(A_Binary(a1)).Should(Equal("0000000000010000"))
+		Ω(A_Binary(a1)).Should(Equal(bLines[0]))
 	})
 	It("C_Binary()", func() {
-		c1 := Command("D=D-M")
-		c2 := Command("D;JGT")
-		Ω(C_Binary(c1)).Should(Equal("1111010011010000"))
-		Ω(C_Binary(c2)).Should(Equal("1110001100000001"))
+		Ω(C_Binary(c1)).Should(Equal(bLines[1]))
+		Ω(C_Binary(c2)).Should(Equal(bLines[2]))
+	})
+	It("ToBinary()", func() {
+		Ω(ToBinary(lines)).Should(Equal(bLines))
 	})
 })
